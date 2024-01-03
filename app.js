0479313a09d5c4c70cb975e319f0d88a7c938e86
negardev92 <negarniakan92@yahoo.com>
2024-01-03 11:29:39 +0330

    part3

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
-
-const f = document.getElementById('form');
-const q = document.getElementById('query');
+const f = document.getElementById("form");
+const q = document.getElementById("query");
 
 let products = [
-  { id:"1" ,model:"laptop_mac",Color: "blue",price:"2000$",photo:"https://colorworksnursery.com/images/thumbs/0000024_apple-macbook-pro-13-inch_550.jpeg"} ,
-  {  id:"2" ,model:"laptop-mac-m2",Color: "red",price:"2500$",photo:"https://colorworksnursery.com/images/thumbs/0000024_apple-macbook-pro-13-inch_550.jpeg"},
-  {  id:"3" ,model:"laptop-mac-m2-Air",Color: "silver",price:"2350$",photo:"https://colorworksnursery.com/images/thumbs/0000024_apple-macbook-pro-13-inch_550.jpeg"}
-]
-f.addEventListener('submit', submitted);
+  {
+    id: "1",
+    model: "laptop_mac",
+    Color: "blue",
+    price: "2000$",
+    photo:
+      "https://colorworksnursery.com/images/thumbs/0000024_apple-macbook-pro-13-inch_550.jpeg",
+  },
+  {
+    id: "2",
+    model: "laptop-mac-m2",
+    Color: "red",
+    price: "2500$",
+    photo:
+      "https://colorworksnursery.com/images/thumbs/0000024_apple-macbook-pro-13-inch_550.jpeg",
+  },
+  {
+    id: "3",
+    model: "laptop-mac-m2-Air",
+    Color: "silver",
+    price: "2350$",
+    photo:
+      "https://colorworksnursery.com/images/thumbs/0000024_apple-macbook-pro-13-inch_550.jpeg",
+  },
+];
+
+f.addEventListener("submit", submitted);
 
 function submitted(event) {
- event.preventDefault();  
-var results = [];
-var toSearch =q.value;
+  event.preventDefault();
+  var results = [];
+  var toSearch = q.value;
 
-for(var i=0; i<products.length; i++) {
-  for(key in products[i]) {
-    if(products[i][key].indexOf(toSearch)!=-1) {
-      results.push(products[i]);
+  for (var i = 0; i < products.length; i++) {
+    for (key in products[i]) {
+      if (products[i][key].indexOf(toSearch) != -1) {
+        results.push(products[i]);
+      }
     }
   }
-}
- 
- let y=document.getElementById('show-search')
- let x= results.find(obj => obj.model);
 
- if (y.style.display === 'none') {
-  y.style.display = 'block';
-  y.innerHTML= `<img  src="${x.photo}"/>
+  let y = document.getElementById("show-search");
+  let x = results.find((obj) => obj.model);
+
+  if (y.style.display === "none") {
+    y.style.display = "block";
+    y.innerHTML = `<img  src="${x.photo}"/>
   <p>model: ${x.model}</br>
    Color: ${x.Color}</br>
   price: ${x.price}</br></p>
-  `
-
-} else {
-  y.style.display = 'none';
-  
-}
-  
-
+  `;
+  } else {
+    y.style.display = "none";
+  }
 }
 
-
-
-
-
-
- products.forEach((product)=>{
- let template =
-document.getElementById("demo").innerHTML+= `
+products.forEach((product) => {
+  let template = (document.getElementById("products-show").innerHTML += `
 
 <p> <img src="${product.photo}" />
 model: ${product.model}</br>
@@ -56,36 +67,52 @@ model: ${product.model}</br>
 price: ${product.price}</br>
 <button id=${product.id} class="btn-add" onclick="AddToCart(event) ">افزودن به سبد</button>
 </p>
- 
+`);
+});
 
-`;
-  
- })
- 
- var cart = [];
- function AddToCart(event) {
-  
+var cart = [];
+function AddToCart(event) {
   event.preventDefault();
   var element = event.target;
-  
-     let x=products.find(obj => obj.id == element.id);
-     cart.push(x);
-    
-     console.log(cart) ;
- 
- }
- 
 
+  let x = products.find((obj) => obj.id == element.id);
+  cart.push(x);
+  return cart;
+}
 
-  
+let cartbtn = document.querySelector(".cart");
+cartbtn.addEventListener("click", showCart);
 
+function showCart(e) {
+  e.preventDefault();
+  show = document.getElementById("cart-show");
 
+  if (show.style.display === "none") {
+    show.style.display = "block";
+    if (cart.length == 0) {
+      show.innerHTML = `
+      <p class="cart-empty"> سبد خالی است </p>`;
+    } else {
+      cart.forEach((cartList) => {
+        show.innerHTML += `
+       <div class="cart-display">
+       <img src="${cartList.photo}" /></br>
+       <p>model: ${cartList.model}</br>
+        Color: ${cartList.Color}</br>
+       price: ${cartList.price}</br>
+       <button id=${cartList.id} class="btn-remove" onclick="RemoveToCart(event)"> حذف </button>
+       </p></div>
+       `;
+      });
+    }
+  } else {
+    show.style.display = "none";
+  }
+}
 
- 
-
- 
-
-
-
-
-  
+function RemoveToCart(event) {
+  event.preventDefault();
+  var removeEl = event.target;
+  let find_id_remove = cart.find((obj) => obj.id == removeEl.id);
+  console.log(find_id_remove);
+}
